da22bd48e1b9d8f13fadb867597c55d4ea3692ea
remzzer <hellokiziche@gmail.com>
2020-07-24 10:41:21 +0200

    final_check

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rrolland <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/21 13:59:41 by rrolland          #+#    #+#             */
-/*   Updated: 2020/07/21 14:43:36 by rrolland         ###   ########.fr       */
+/*   Updated: 2020/07/24 10:40:22 by rrolland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list		*element;
+	t_list		*new_list;
 
+	new_list = NULL;
+	while(lst != NULL)
+	{
+		element = ft_lstnew((*f)(lst->content));
+		if (!element)
+		{
+			ft_clear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, element);
+		lst = lst->next;
+	}
+	return (new_list);
 }
